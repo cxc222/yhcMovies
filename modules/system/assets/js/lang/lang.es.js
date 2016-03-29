1bcc55736c152b81beeb4121085951f1d4590e92
cxc222 <cxc359170248@vip.qq.com>
2016-03-29 17:34:23 +0800

    no message

--- modules/system/assets/js/lang/lang.es.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file has been compiled from: /modules/system/lang/es/client.php
- */
-if ($.oc === undefined) $.oc = {}
-if ($.oc.langMessages === undefined) $.oc.langMessages = {}
-$.oc.langMessages['es'] = $.extend(
-    $.oc.langMessages['es'] || {},
-    {"markdowneditor":{"formatting":"Formateo","quote":"Cita","code":"C\u00f3digo","header1":"Encabezado 1","header2":"Encabezado 2","header3":"Encabezado 3","header4":"Encabezado 4","header5":"Encabezado 5","header6":"Encabezado 6","bold":"Negrita","italic":"Cursiva","unorderedlist":"Lista Desordenada","orderedlist":"Lista Ordenada","video":"Video","image":"Imagen","link":"V\u00ednculo","horizontalrule":"Insertar Regla Horizontal","fullscreen":"Pantalla completa","preview":"Previsualizar"},"mediamanager":{"insert_link":"Insertar Media V\u00ednculo","insert_image":"Insertar Media Imagen","insert_video":"Insertar Media Video","insert_audio":"Insertar Media Audio","invalid_file_empty_insert":"Por favor seleccione archivo para insertar v\u00ednculo.","invalid_file_single_insert":"Por favor seleccione un solo archivo.","invalid_image_empty_insert":"Por favor seleccione una imagen(es) para insertar.","invalid_video_empty_insert":"Por favor seleccione un archivo de video para insertar.","invalid_audio_empty_insert":"Por favor seleccione un archivo de audio para insertar."},"alert":{"confirm_button_text":"OK","cancel_button_text":"Cancelar"}}
-);

77cb81d9c27f0f8ed66e18155ceb9f7d8c406034
cxc222 <cxc359170248@vip.qq.com>
2016-07-06 11:46:53 +0800

    加入统计

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -8,4 +8,5 @@
         如您认为本站页面信息侵犯了您的权益，请附上版权证明邮件告知，在收到邮件后24小时内删除<br>
         邮箱: admin#yhec.cn (请将#换成@)
     </p>
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1259838149'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/stat.php%3Fid%3D1259838149%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
 </footer>

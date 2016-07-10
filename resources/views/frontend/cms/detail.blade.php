d43f4255810e15b62f847d95a0f68073af989897
cxc222 <cxc359170248@vip.qq.com>
2016-07-10 18:43:59 +0800

    分享代码

--- resources/views/frontend/cms/detail.blade.php
+++ resources/views/frontend/cms/detail.blade.php
@@ -29,6 +29,9 @@
                                 <strong>片长：</strong>{{ $article->film_long }}<br>
                                 <strong>国家：</strong>{{ $article->country }}<br>
                                 <strong>剧情简介：</strong>{!! $article->content !!}<br>
+
+                                <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+                                <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"16"},"share":{},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"16"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
                             </div>
                         </div>
                         <div class="panel panel-default" style="margin-top:10px;" id="down4">
